ab1ae9b85348eebcd4e0c94839d1163706e081b9
Sajad Abdollahi <sajad.abdollahi1380@gmail.com>
2022-03-21 13:47:55 +0330

    fix: refactor

--- lib/src/animated_snack_bar.dart
+++ lib/src/animated_snack_bar.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/raw_animated_snack_bar.dart';
+
 showAnimatedSnackBar(BuildContext context, {Duration? duration}) async {
   duration = duration ?? const Duration(seconds: 8);
   final state = Navigator.of(context).overlay!;
   late OverlayEntry entry;
   entry = OverlayEntry(
-    builder: (_) => _RawAnimatedSnackBar(
+    builder: (_) => RawAnimatedSnackBar(
       duration: duration!,
       onRemoved: () {
         entry.remove();
@@ -19,89 +21,3 @@ showAnimatedSnackBar(BuildContext context, {Duration? duration}) async {
   );
 }
 
-class _RawAnimatedSnackBar extends StatefulWidget {
-  const _RawAnimatedSnackBar(
-      {Key? key,
-      required this.duration,
-      required this.child,
-      required this.onRemoved,
-      this.backgroundColor})
-      : super(key: key);
-
-  final Duration duration;
-  final Widget child;
-  final VoidCallback onRemoved;
-  final Color? backgroundColor;
-
-  @override
-  State<_RawAnimatedSnackBar> createState() => _RawAnimatedSnackBarState();
-}
-
-class _RawAnimatedSnackBarState extends State<_RawAnimatedSnackBar> {
-  bool isVisible = false;
-  bool removed = false;
-
-  final duration = const Duration(milliseconds: 400);
-
-  final GlobalKey positionedKey = GlobalKey();
-
-  void remove() {
-    if (mounted && removed == false) {
-      widget.onRemoved();
-    } else {
-      removed = true;
-    }
-  }
-
-  @override
-  void initState() {
-    Future.delayed(
-      const Duration(milliseconds: 200),
-      () {
-        setState(() => isVisible = true);
-      },
-    );
-    Future.delayed(
-      Duration(milliseconds: widget.duration.inMilliseconds - 2000),
-      () {
-        if (mounted) {
-          setState(() => isVisible = false);
-          Future.delayed(const Duration(seconds: 2), () {
-            remove();
-          });
-        }
-      },
-    );
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedPositioned(
-      key: positionedKey,
-      duration: duration,
-      top: isVisible ? 70 : -100,
-      left: 35,
-      right: 35,
-      child: AnimatedOpacity(
-        opacity: isVisible ? 1 : .3,
-        duration: const Duration(milliseconds: 1000),
-        child: Material(
-          color: Colors.transparent,
-          borderRadius: BorderRadius.circular(10),
-          child: Container(
-            color: widget.backgroundColor,
-            height: 100,
-            child: widget.child,
-          ),
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/src/widgets/raw_animated_snack_bar.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class RawAnimatedSnackBar extends StatefulWidget {
+  const RawAnimatedSnackBar(
+      {Key? key,
+      required this.duration,
+      required this.child,
+      required this.onRemoved,
+      this.backgroundColor})
+      : super(key: key);
+
+  final Duration duration;
+  final Widget child;
+  final VoidCallback onRemoved;
+  final Color? backgroundColor;
+
+  @override
+  State<RawAnimatedSnackBar> createState() => RawAnimatedSnackBarState();
+}
+
+class RawAnimatedSnackBarState extends State<RawAnimatedSnackBar> {
+  bool isVisible = false;
+  bool removed = false;
+
+  final duration = const Duration(milliseconds: 400);
+
+  final GlobalKey positionedKey = GlobalKey();
+
+  void remove() {
+    if (mounted && removed == false) {
+      widget.onRemoved();
+    } else {
+      removed = true;
+    }
+  }
+
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(milliseconds: 200),
+      () {
+        setState(() => isVisible = true);
+      },
+    );
+    Future.delayed(
+      Duration(milliseconds: widget.duration.inMilliseconds - 2000),
+      () {
+        if (mounted) {
+          setState(() => isVisible = false);
+          Future.delayed(const Duration(seconds: 2), () {
+            remove();
+          });
+        }
+      },
+    );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPositioned(
+      key: positionedKey,
+      duration: duration,
+      top: isVisible ? 70 : -100,
+      left: 35,
+      right: 35,
+      child: AnimatedOpacity(
+        opacity: isVisible ? 1 : .3,
+        duration: const Duration(milliseconds: 1000),
+        child: Material(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            color: widget.backgroundColor,
+            height: 100,
+            child: widget.child,
+          ),
+        ),
+      ),
+    );
+  }
+}
