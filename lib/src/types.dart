72d2d7b6f2d952fb722b7f721a9ab557f32a091f
Sajad Abdollahi <sajad.abdollahi1380@gmail.com>
2022-09-09 21:13:31 +0430

    feat: add `MultipleSnackBarStrategy`

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../animated_snack_bar.dart';
+
 /// Snack bar types for [AnimatedSnackBar]
 ///
 /// There will be a pre defined color and icon for each one of
@@ -21,3 +25,85 @@ enum DesktopSnackBarPosition {
 
 /// Snack bar position on mobile devices (screens with width <= 600)
 enum MobileSnackBarPosition { top, bottom }
+
+abstract class MultipleSnackBarStrategy {
+  double computeDy(List<AnimatedSnackBar> snackBars, AnimatedSnackBar self);
+  List<AnimatedSnackBar> onAdd(
+      List<AnimatedSnackBar> snackBars, AnimatedSnackBar self);
+}
+
+class ColumnSnackBarStrategy implements MultipleSnackBarStrategy {
+  final double gap;
+  final int maxSnackBars;
+
+  const ColumnSnackBarStrategy({this.gap = 8, this.maxSnackBars = 4});
+
+  @override
+  double computeDy(List<AnimatedSnackBar> snackBars, AnimatedSnackBar self) {
+    final olderBars = snackBars
+        .where(
+          (element) =>
+              element.info.key.currentState?.widget.desktopSnackBarPosition ==
+                  self.desktopSnackBarPosition &&
+              element.info.key.currentState!.isVisible &&
+              element.info.createdAt.isBefore(self.info.createdAt),
+        )
+        .toList();
+
+    return olderBars.fold<double>(0, (initialValue, element) {
+      final box =
+          element.info.key.currentContext!.findRenderObject() as RenderBox;
+
+      return initialValue + box.size.height + gap;
+    });
+  }
+
+  @override
+  List<AnimatedSnackBar> onAdd(
+      List<AnimatedSnackBar> snackBars, AnimatedSnackBar self) {
+    return snackBars;
+  }
+}
+
+class RemoveSnackBarStrategy implements MultipleSnackBarStrategy {
+  @override
+  double computeDy(List<AnimatedSnackBar> snackBars, AnimatedSnackBar self) {
+    return 0;
+  }
+
+  @override
+  List<AnimatedSnackBar> onAdd(
+      List<AnimatedSnackBar> snackBars, AnimatedSnackBar self) {
+    bool shouldRemove(AnimatedSnackBar element) =>
+        element.info.createdAt.isBefore(self.info.createdAt) &&
+        element.mobileSnackBarPosition == self.mobileSnackBarPosition &&
+        element.desktopSnackBarPosition == self.desktopSnackBarPosition;
+
+    snackBars
+        .where(
+      shouldRemove,
+    )
+        .forEach((element) {
+      element.info.key.currentState!.fadeOut().then(
+            (value) => element.remove(),
+          );
+    });
+
+    snackBars.removeWhere(shouldRemove);
+
+    return snackBars;
+  }
+}
+
+class StackSnackBarStrategy implements MultipleSnackBarStrategy {
+  @override
+  double computeDy(List<AnimatedSnackBar> snackBars, AnimatedSnackBar self) {
+    return 0;
+  }
+
+  @override
+  List<AnimatedSnackBar> onAdd(
+      List<AnimatedSnackBar> snackBars, AnimatedSnackBar self) {
+    return snackBars;
+  }
+}
